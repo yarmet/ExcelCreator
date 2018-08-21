881e8cb68f87eff57a6b64925bb8104544d74f67
ruslan <yarmet84@mail.ru>
2018-08-21 22:00:40 +0300

    Отрефакторил

--- src/main/java/ruslan/Main.java
+++ src/main/java/ruslan/Main.java
@@ -1,12 +1,11 @@
 package ruslan;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
+
+import org.apache.poi.ss.usermodel.FillPatternType;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFColor;
-
 import java.io.IOException;
-import java.util.Date;
+
 
 /**
  * Created by ruslan on 22.05.2017.
@@ -19,41 +18,40 @@ public class Main {
         XlsWriter xlsWriter = new XlsWriter(2);
 
 
-        XSSFCellStyle greenBackground = xlsWriter.createXssfCellStyle();
+        XSSFCellStyle greenBackground = xlsWriter.createXSSFCellStyle();
         greenBackground.setFillForegroundColor(new XSSFColor(new java.awt.Color(50, 150, 50)));
-        greenBackground.setFillPattern(CellStyle.SOLID_FOREGROUND);
+        greenBackground.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 
-        XSSFCellStyle redBackground = xlsWriter.createXssfCellStyle();
+        XSSFCellStyle redBackground = xlsWriter.createXSSFCellStyle();
         redBackground.setFillForegroundColor(new XSSFColor(new java.awt.Color(150, 50, 50)));
-        redBackground.setFillPattern(CellStyle.SOLID_FOREGROUND);
+        redBackground.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         //--------------------------------------------------------------------------------------------------------------
 
         xlsWriter.changeSheet(0);
-        xlsWriter.establishSheetName("лист1");
-
-        String[] columnNames = {"column1", "column2", "column3"};
-
-        xlsWriter.mergeCells(10, 12, 3, 3);
-        xlsWriter.createheader(columnNames);
+        xlsWriter.establishCurrentSheetName("лист1");
 
 
         xlsWriter.createRow();
-        xlsWriter.createCell("cell1");
-        xlsWriter.createCell(true);
-
-        Cell cell1 = xlsWriter.createCell(new Date());
-        cell1.setCellStyle(redBackground);
-        xlsWriter.finishRow();
-
+        xlsWriter.createCell(123);
+        xlsWriter.createCell(23);
 
         xlsWriter.createRow();
-        xlsWriter.createCell("cell4");
-        xlsWriter.createCell(false);
+        xlsWriter.createCell(33);
+        xlsWriter.createCell(44);
 
-        Cell cell = xlsWriter.createCell(new Date());
-        cell.setCellStyle(greenBackground);
 
-        xlsWriter.finishRow();
+        xlsWriter.changeSheet(1);
+
+        xlsWriter.createRow();
+        xlsWriter.createCell(77);
+        xlsWriter.createCell(88);
+
+
+
+        xlsWriter.changeSheet(0);
+
+        xlsWriter.createCell(11);
+        xlsWriter.createCell(11);
 
         xlsWriter.saveInFile("myexcel");
     }
--- src/main/java/ruslan/XlsWriter.java
+++ src/main/java/ruslan/XlsWriter.java
@@ -6,21 +6,17 @@ package ruslan;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.function.Consumer;
 
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.RichTextString;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFColor;
 
 
 /**
@@ -28,98 +24,105 @@ import org.apache.poi.xssf.usermodel.XSSFColor;
  */
 public class XlsWriter {
 
-    private Row row = null;
     private final SXSSFWorkbook workbook;
-    private int currSheet = 0;
-    private final int[] rowNumber;
-    private SXSSFSheet[] sheets = null;
-    private int cellNumber = 0;
-
+    private int currSheetNumber = 0;
+    private CustomSheetWrapper[] customSheetWrappers;
 
     public XlsWriter(int sheetCount) {
-        rowNumber = new int[sheetCount];
         workbook = new SXSSFWorkbook();
-        sheets = new SXSSFSheet[sheetCount];
+        customSheetWrappers = new CustomSheetWrapper[sheetCount];
         for (int i = 0; i < sheetCount; i++) {
-            rowNumber[i] = 1;
-            sheets[i] = (SXSSFSheet) workbook.createSheet();
+            customSheetWrappers[i] = new CustomSheetWrapper(workbook.createSheet());
         }
     }
 
-    public void establishSheetName(String name) {
-        workbook.setSheetName(currSheet, name);
-    }
-
-
-    public void createheader(String[] headers) {
-        Row rowhead = sheets[currSheet].createRow(0);
-        for (int i = 0; i < headers.length; i++) {
-            rowhead.createCell(i).setCellValue(headers[i]);
-        }
+    public void establishCurrentSheetName(String name) {
+        workbook.setSheetName(currSheetNumber, name);
     }
 
     public void changeSheet(int sheetNumber) {
-        currSheet = sheetNumber;
+        if (sheetNumber < 0 || sheetNumber >= customSheetWrappers.length) {
+            throw new IllegalArgumentException("wrong sheet number");
+        }
+        currSheetNumber = sheetNumber;
     }
-
-
-    private Cell createCellAndGet(Consumer<Cell> consumer) {
-        Cell cell = row.createCell(cellNumber++);
-        consumer.accept(cell);
-        return cell;
-    }
-
 
     public Cell createCell(double value) {
-        return createCellAndGet((cell) -> cell.setCellValue(value));
+        return customSheetWrappers[currSheetNumber].createCellAngGet((cell) -> cell.setCellValue(value));
     }
 
     public Cell createCell(String value) {
-        return createCellAndGet((cell) -> cell.setCellValue(value));
+        return customSheetWrappers[currSheetNumber].createCellAngGet((cell) -> cell.setCellValue(value));
     }
 
     public Cell createCell(boolean value) {
-        return createCellAndGet((cell) -> cell.setCellValue(value));
+        return customSheetWrappers[currSheetNumber].createCellAngGet((cell) -> cell.setCellValue(value));
     }
 
     public Cell createCell(Date value) {
-        return createCellAndGet((cell) -> cell.setCellValue(value));
+        return customSheetWrappers[currSheetNumber].createCellAngGet((cell) -> cell.setCellValue(value));
     }
 
     public Cell createCell(Calendar value) {
-        return createCellAndGet((cell) -> cell.setCellValue(value));
+        return customSheetWrappers[currSheetNumber].createCellAngGet((cell) -> cell.setCellValue(value));
     }
 
     public Cell createCell(RichTextString value) {
-        return createCellAndGet((cell) -> cell.setCellValue(value));
+        return customSheetWrappers[currSheetNumber].createCellAngGet((cell) -> cell.setCellValue(value));
     }
-
 
     public void mergeCells(int firstRow, int lastRow, int firstColumn, int lastColumn) {
-        sheets[currSheet].addMergedRegion(new CellRangeAddress(firstRow, lastRow, firstColumn, lastColumn));
+        customSheetWrappers[currSheetNumber].mergeCells(firstRow, lastRow, firstColumn, lastColumn);
     }
 
-
-    public void finishRow() {
-        ++rowNumber[currSheet];
-        cellNumber = 0;
-    }
-
-
-    public XSSFCellStyle createXssfCellStyle() {
+    public XSSFCellStyle createXSSFCellStyle() {
         return (XSSFCellStyle) workbook.createCellStyle();
     }
 
-
     public void createRow() {
-        row = sheets[currSheet].createRow(rowNumber[currSheet]);
+        customSheetWrappers[currSheetNumber].createRow();
     }
 
+    public int getRowNumber() {
+        return customSheetWrappers[currSheetNumber].getRowNumber();
+    }
 
     public void saveInFile(String fileName) throws IOException {
         try (FileOutputStream fileOut = new FileOutputStream(fileName.concat(".xlsx"))) {
             workbook.write(fileOut);
         }
+    }
+
+
+    private class CustomSheetWrapper {
+        private int currentRowNumber = 0;
+        private int currentCellNumber = 0;
+        private Row row;
+        private SXSSFSheet sheet;
+
+        public CustomSheetWrapper(SXSSFSheet sheet) {
+            this.sheet = sheet;
+        }
+
+        private void createRow() {
+            row = sheet.createRow(currentRowNumber++);
+            currentCellNumber = 0;
+        }
+
+        private Cell createCellAngGet(Consumer<Cell> consumer) {
+            Cell cell = row.createCell(currentCellNumber++);
+            consumer.accept(cell);
+            return cell;
+        }
+
+        private void mergeCells(int firstRow, int lastRow, int firstColumn, int lastColumn) {
+            sheet.addMergedRegion(new CellRangeAddress(firstRow, lastRow, firstColumn, lastColumn));
+        }
+
+        private int getRowNumber() {
+            return currentRowNumber;
+        }
+
     }
 
 }
